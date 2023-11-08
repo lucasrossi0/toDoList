e983efd33a5a3f4026400d18a496a4982048395a
lucasrossi0 <lucasrossi502@gmail.com>
2023-11-08 09:39:47 -0300

    Diminuindo tamanho do codigo e comentando tudo

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,28 +1,38 @@
-let btnAdd = document.getElementById("btnAdd");
-let inpTask = document.getElementById("inpTask");
-let taskContainer = document.getElementById("test");
+document.addEventListener("DOMContentLoaded", () => {
+  let btnAdd = document.getElementById("btnAdd");
+  let inpTask = document.getElementById("inpTask");
+  let taskContainer = document.getElementById("container");
+  // getting the dom objects of the page
 
-btnAdd.addEventListener("click", () => {
-  let div = document.createElement("div");
-  div.id = "taskContainer";
+  btnAdd.addEventListener("click", () => {
+    let div = document.createElement("div");
+    let btn = document.createElement("button");
+    let p = document.createElement("p");
+    // creating html tags fot the to do tasks
 
-  let button = document.createElement("button");
-  button.innerText = "Remove";
-  button.id = "btnRemove";
+    add(div, p, btn);
+    // calling the function that adds those html tags to the dom
 
-  let p = document.createElement("p");
-  taskContainer.appendChild(div);
+    btn.addEventListener("click", () => {
+      remove(div);
+    });
+    // onclick the remove btn is being used
+  });
 
-  p.innerText = inpTask.value;
+  function remove(btn) {
+    btn.remove();
+  }
+  // to remove the task
 
-  div.appendChild(p);
-  div.appendChild(button);
-
-  button.onclick = () => {
-    remove(div);
-  };
+  function add(div, p, btn) {
+    div.id = "taskContainer";
+    btn.innerText = "Remove";
+    btn.id = "btnRemove";
+    p.innerText = inpTask.value;
+    div.appendChild(p);
+    div.appendChild(btn);
+    taskContainer.appendChild(div);
+  }
+  // this function associate the html tags created before with the ids, text content
+  // this function adds those html tags inside the dom
 });
-
-function remove(btn) {
-  btn.remove();
-}
